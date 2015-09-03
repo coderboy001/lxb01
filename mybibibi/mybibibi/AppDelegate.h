612d5f7d10a6f764a0859da6d138efe606501c53
lxb <coderboy001@gmail.com>
2015-09-03 16:45:39 +0800

    初始化项目

---
+++ mybibibi/mybibibi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mybibibi
+//
+//  Created by lxb on 15/9/3.
+//  Copyright (c) 2015年 lxb.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mybibibi/mybibibi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mybibibi
+//
+//  Created by lxb on 15/9/3.
+//  Copyright (c) 2015年 lxb.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
